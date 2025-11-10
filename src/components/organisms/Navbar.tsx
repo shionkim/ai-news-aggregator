a83461d3e5eb8ffb98978876d1292d725a9d85cf
Shion Kim <shionkim@icloud.com>
2025-11-10 14:31:33 +0100

    disable lozy loading for logo

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -20,6 +20,7 @@ export default function Navbar() {
             width={170}
             height={33}
             alt="Logo"
+            priority
           />
         </div>
 
--- src/components/organisms/NavbarMobile.tsx
+++ src/components/organisms/NavbarMobile.tsx
@@ -19,7 +19,13 @@ export default function NavbarMobile() {
     <>
       {/* Mobile Header (visible when menu closed) */}
       <div className="flex items-center justify-between px-4 py-3 md:hidden sm:px-8 border-b border-gray-200">
-        <Image src="/assets/logo/logo.svg" width={140} height={33} alt="Logo" />
+        <Image
+          src="/assets/logo/logo.svg"
+          width={140}
+          height={33}
+          alt="Logo"
+          priority
+        />
         <button onClick={toggleMenu}>
           <Menu01 className="w-6 h-6" />
         </button>
@@ -35,6 +41,7 @@ export default function NavbarMobile() {
               width={140}
               height={33}
               alt="Logo"
+              priority
             />
             <button onClick={toggleMenu}>
               <X className="w-6 h-6" />
