738872390f9b66467b58ab3a3aed6426890f9dc3
shionkim <shionkim@icloud.com>
2026-01-21 15:55:36 +0100

    fix width

--- src/components/organisms/LanguageSelector.tsx
+++ src/components/organisms/LanguageSelector.tsx
@@ -22,7 +22,7 @@ export default function LanguageSelector() {
 
       <div className="fixed inset-x-0 top-0 h-dvh flex flex-col gap-8 z-10 md:sticky md:top-0 md:h-dvh">
         <div className="md:hidden"></div>
-        <div className="border-t rounded-t-2xl border-gray-200 text-gray-700 bg-white md:w-2xs md:border-r md:border-t-0 md:rounded-none overflow-hidden">
+        <div className="border-t rounded-t-2xl border-gray-200 text-gray-700 bg-white md:w-xs md:border-r md:border-t-0 md:rounded-none overflow-hidden">
           <div className="flex flex-col h-full">
             {/* Header */}
             <div className="flex flex-col px-4 py-6 gap-6 border-b border-gray-200 bg-white relative">
