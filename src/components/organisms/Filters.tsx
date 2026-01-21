059200ac5075abdb054cc9e770b4f18f6e59e553
shionkim <shionkim@icloud.com>
2026-01-21 17:08:12 +0100

    css tidy up

--- src/components/organisms/Filters.tsx
+++ src/components/organisms/Filters.tsx
@@ -123,11 +123,11 @@ export default function Filters() {
   return (
     <div>
       <div
-        className="fixed h-svh inset-0 bg-black/50 backdrop-blur-xs md:hidden"
+        className="fixed inset-0 bg-black/50 backdrop-blur-xs md:hidden"
         onClick={closeFilters}
       />
 
-      <div className="fixed inset-x-0 bottom-0 flex flex-col gap-8 md:sticky md:top-0 z-10 overflow-hidden border-t rounded-t-2xl border-gray-200 h-[calc(100dvh-2rem)] md:h-svh bg-white md:w-xs md:border-r md:rounded-none md:border-t-0">
+      <div className="fixed inset-x-0 bottom-0 flex flex-col gap-8  z-10 overflow-hidden border-t rounded-t-2xl border-gray-200 bg-white h-[calc(100dvh-2rem)] md:sticky md:h-svh md:top-0 md:w-xs md:border-r md:rounded-none md:border-t-0">
         <div className="flex flex-col h-full">
           {/* Header */}
           <div className="px-4 py-6 border-b border-gray-200 relative bg-white">
